e37032ef08c0d99c34bc55bd93a4f4e68f0d5220
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-05-06 14:42:35 -0300

    Feat: API Key on report and order routes

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const apiKeyMiddleware = require("../config/apiKey");
 
+router.use(apiKeyMiddleware);
 router.get('/', orderController.getOrders);
 router.get('/:id', orderController.getOrderById);
 router.post('/', orderController.createOrder);
--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,8 +1,9 @@
-const express = require ("express");
+ const express = require ("express");
  const router = express.Router();
- 
  const reportController = require("../controllers/reportController");
- 
+ const apiKeyMiddleware = require("../config/apiKey");
+
+ router.use(apiKeyMiddleware);
  router.get("/report/pdf/order", reportController.exportOrderPdf);
  router.get("/report/pdf/client", reportController.exportClientsPdf);
  
